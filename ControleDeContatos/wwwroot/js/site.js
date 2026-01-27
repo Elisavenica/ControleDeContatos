b1a32dab920dc2a79bfb12aecaf600b724071375
Elisa Vênica <elisac02@outlook.com>
2026-01-27 10:37:10 -0300

    Criação de tabela de Usuario

--- ControleDeContatos/wwwroot/js/site.js
+++ ControleDeContatos/wwwroot/js/site.js
@@ -1,10 +1,33 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
 
-// Write your JavaScript code.
+    let table = $('#table-contatos').DataTable({
+        ordering: true,
+        paging: true,
+        searching: true,
+        language: {
+            emptyTable: "Nenhum registro encontrado na tabela",
+            info: "Mostrar _START_ até _END_ de _TOTAL_ registros",
+            infoEmpty: "Mostrar 0 até 0 de 0 Registros",
+            infoFiltered: "(Filtrar de _MAX_ total registros)",
+            lengthMenu: "Mostrar _MENU_ registros por página",
+            loadingRecords: "Carregando...",
+            processing: "Processando...",
+            zeroRecords: "Nenhum registro encontrado",
+            search: "Pesquisar",
+            paginate: {
+                next: "Próximo",
+                previous: "Anterior",
+              
+            },
+            aria: {
+                sortAscending: ": Ordenar colunas de forma ascendente",
+                sortDescending: ": Ordenar colunas de forma descendente"
+            }
+        }
+    });
 
-let table = new DataTable('#table-contatos');
+    $('.close-alert').click(function () {
+        $('.alert').hide();
+    });
 
-$('.close-alert').click(function () {
-    $('.alert').hide('hide');
 });
