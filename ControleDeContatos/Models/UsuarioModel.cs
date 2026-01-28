271c92263eff77b2617885e46d398e28b64ab784
Elisa Vênica <elisac02@outlook.com>
2026-01-27 21:12:49 -0300

    inclusão do Usuario Model

--- ControleDeContatos/Models/UsuarioModel.cs
+++ ControleDeContatos/Models/UsuarioModel.cs
@@ -9,6 +9,14 @@ namespace ControleDeContatos.Models
         public string Nome { get; set; }
         public string Login { get; set; }
         public string Email { get; set; }
+        public string Celular { get; set; } 
+        public string Rua { get; set; } 
+        public string Numero { get; set; } 
+        public string Bairro { get; set; } 
+        public string rua { get; set; } 
+        public string Cidade { get; set; } 
+        public string Estado { get; set; } 
+        public string Cep { get; set; } 
         public PerfilEnum Perfil { get; set; }
         public string Senha { get; set; }
         public DateTime DataCadastro { get; set; }
