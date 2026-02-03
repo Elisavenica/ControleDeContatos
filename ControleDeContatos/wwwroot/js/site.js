1d01a6bd518986c26cc6d193887830c14c62fa91
Elisa Vênica <elisac02@outlook.com>
2026-02-03 09:47:56 -0300

    Alterações

--- ControleDeContatos/wwwroot/js/site.js
+++ ControleDeContatos/wwwroot/js/site.js
@@ -1,31 +1,64 @@
 ﻿$(document).ready(function () {
 
-    let table = $('#table-contatos').DataTable({
-        ordering: true,
-        paging: true,
-        searching: true,
-        language: {
-            emptyTable: "Nenhum registro encontrado na tabela",
-            info: "Mostrar _START_ até _END_ de _TOTAL_ registros",
-            infoEmpty: "Mostrar 0 até 0 de 0 Registros",
-            infoFiltered: "(Filtrar de _MAX_ total registros)",
-            lengthMenu: "Mostrar _MENU_ registros por página",
-            loadingRecords: "Carregando...",
-            processing: "Processando...",
-            zeroRecords: "Nenhum registro encontrado",
-            search: "Pesquisar",
-            paginate: {
-                next: "Próximo",
-                previous: "Anterior",
-              
-            },
-            aria: {
-                sortAscending: ": Ordenar colunas de forma ascendente",
-                sortDescending: ": Ordenar colunas de forma descendente"
+    // Tabela de CONTATOS
+    if ($('#table-contatos').length) {
+        $('#table-contatos').DataTable({
+            ordering: true,
+            paging: true,
+            searching: true,
+            language: {
+                emptyTable: "Nenhum registro encontrado na tabela",
+                info: "Mostrar _START_ até _END_ de _TOTAL_ registros",
+                infoEmpty: "Mostrar 0 até 0 de 0 Registros",
+                infoFiltered: "(Filtrar de _MAX_ total registros)",
+                lengthMenu: "Mostrar _MENU_ registros por página",
+                loadingRecords: "Carregando...",
+                processing: "Processando...",
+                zeroRecords: "Nenhum registro encontrado",
+                search: "Pesquisar",
+                paginate: {
+                    next: "Próximo",
+                    previous: "Anterior",
+                },
+                aria: {
+                    sortAscending: ": Ordenar colunas de forma ascendente",
+                    sortDescending: ": Ordenar colunas de forma descendente"
+                }
             }
-        }
-    });
+        });
+    }
 
+    // Tabela de USUÁRIOS
+    if ($('#table-Usuarios').length) {
+        $('#table-Usuarios').DataTable({
+            ordering: true,
+            paging: true,
+            searching: true,
+            language: {
+                emptyTable: "Nenhum registro encontrado na tabela",
+                info: "Mostrar _START_ até _END_ de _TOTAL_ registros",
+                infoEmpty: "Mostrar 0 até 0 de 0 Registros",
+                infoFiltered: "(Filtrar de _MAX_ total registros)",
+                lengthMenu: "Mostrar _MENU_ registros por página",
+                loadingRecords: "Carregando...",
+                processing: "Processando...",
+                zeroRecords: "Nenhum registro encontrado",
+                search: "Pesquisar",
+                paginate: {
+                    next: "Próximo",
+                    previous: "Anterior",
+                },
+                aria: {
+                    sortAscending: ": Ordenar colunas de forma ascendente",
+                    sortDescending: ": Ordenar colunas de forma descendente"
+                }
+            }
+        });
+    }
+
+
+
+    // Fechar alertas
     $('.close-alert').click(function () {
         $('.alert').hide();
     });
